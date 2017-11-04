a4114fb668bfdedec68a956952d61d41c845954e
Ashutosh Singh <singhashutosh8179@gmail.com>
2017-11-04 08:04:19 +0530

    commit

---
+++ News Paper/Article.swift
@@ -0,0 +1,20 @@
+//
+//  Article.swift
+//  News Paper
+//
+//  Created by Ashutosh Singh on 01/08/17.
+//  Copyright © 2017 Ashutosh Singh. All rights reserved.
+//
+
+import UIKit
+
+class Article: NSObject {
+    
+    
+    var headline: String?
+    var desc: String?
+    var author: String?
+    var url: String?
+    var imageUrl: String?
+    
+}
